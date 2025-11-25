6c4d949f5da95c520fff97afbe4b1a46fe3837d9
Hpayeur <235752572+Hpayeur@users.noreply.github.com>
2025-11-25 12:31:20 -0700

    variables and Data types

--- main.js
+++ main.js
@@ -2,3 +2,12 @@
 console.log ("Hello, World!"); 
 console.error('Error message');
 console.warn('Warning message');
+
+// Variables= var?, let, const//
+let age = 25;
+age = 26;
+console.log(age);
+
+//Let example //
+// let score;    socre = 10; console.log(score); //
+//Data Types= String, number, Boolean, Null, Undefined, and Symbol//
